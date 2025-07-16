950f6ddf6d26abc8abb4a2f914a34063afc6faed
Marek Pankowski <17934750+mkpanq@users.noreply.github.com>
2025-07-16 13:07:18 +0200

    very basic theme switcher - needs improvements

--- src/components/background.tsx
+++ src/components/background.tsx
@@ -3,18 +3,32 @@ import FOG from "vanta/dist/vanta.fog.min";
 import ThemeSwitcher from "./themeSwitcher";
 
 const Background = ({ children }: { children: React.ReactNode }) => {
-  const [backgroundEffect, setBackgroundEffect] = useState(null);
   const bgRef = useRef(null);
+  const [backgroundEffect, setBackgroundEffect] = useState(null);
+  const [darkMode, setDarkMode] = useState(
+    window.matchMedia("(prefers-color-scheme: dark)").matches ?? false,
+  );
 
+  useEffect(() => {
+    console.log("Theme mode changed:", darkMode);
+    setBackgroundEffect(
+      FOG({
+        el: bgRef.current,
+        ...backgroundColorSet[darkMode ? "dark" : "light"],
+        blurFactor: 0.8,
+        speed: 2,
+        zoom: 0.7,
+      }),
+    );
+  }, [darkMode]);
+
+  // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
   useEffect(() => {
     if (!backgroundEffect)
       setBackgroundEffect(
         FOG({
           el: bgRef.current,
-          highlightColor: 0x9fb3bf,
-          midtoneColor: 0x16132b,
-          lowlightColor: 0x9fb3bf,
-          baseColor: 0x16132b,
+          ...backgroundColorSet[darkMode ? "dark" : "light"],
           blurFactor: 0.8,
           speed: 2,
           zoom: 0.7,
@@ -25,19 +39,38 @@ const Background = ({ children }: { children: React.ReactNode }) => {
       // @ts-ignore
       if (backgroundEffect) backgroundEffect.destroy();
     };
-  }, [backgroundEffect]);
+  }, []);
 
   return (
     <div ref={bgRef} className="absolute top-0 left-0 w-full h-full -z-1">
       <div className="z-1">
-        <ThemeSwitcher />
+        <ThemeSwitcher
+          backgroundState={darkMode}
+          backgroundStateSwitcher={setDarkMode}
+        />
         {children}
       </div>
     </div>
   );
 };
 
+const backgroundColorSet = {
+  dark: {
+    highlightColor: 0x9fb3bf,
+    midtoneColor: 0x16132b,
+    lowlightColor: 0x9fb3bf,
+    baseColor: 0x16132b,
+  },
+  light: {
+    highlightColor: 0xf5d7a6,
+    midtoneColor: 0xf5d7a6,
+    lowlightColor: 0xf5d7a6,
+    baseColor: 0xfffdfa,
+  },
+};
+
 export default Background;
+
 // Light mode colors:
 // highlightColor: 0xf5d7a6,
 // midtoneColor: 0xf5d7a6,
--- src/components/themeSwitcher.tsx
+++ src/components/themeSwitcher.tsx
@@ -1,19 +1,15 @@
-import { useEffect, useState } from "react";
-
-const ThemeSwitcher = () => {
-  const [darkMode, setDarkMode] = useState(
-    window.matchMedia("(prefers-color-scheme: dark)").matches ?? false,
-  );
-
-  useEffect(() => {
-    console.log("Theme mode changed:", darkMode);
-  }, [darkMode]);
-
+const ThemeSwitcher = ({
+  backgroundState,
+  backgroundStateSwitcher,
+}: {
+  backgroundState: boolean;
+  backgroundStateSwitcher: (backgroundState: boolean) => void;
+}) => {
   return (
     <button
       type="button"
       className="absolute top-0 right-0 rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-      onClick={() => setDarkMode(!darkMode)}
+      onClick={() => backgroundStateSwitcher(!backgroundState)}
     >
       Switch theme
     </button>
