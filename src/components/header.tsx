880572302559f4a734e0f88a3db4c703fdc94c56
Marek Pankowski <17934750+mkpanq@users.noreply.github.com>
2025-07-21 13:03:24 +0200

    Initital header

---
+++ src/components/header.tsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <div className="flex justify-between font-medium text-black/50 text-3xl px-3 py-3">
+      <p className="font-semibold">mkpanq.com</p>
+      <div className="flex space-x-10">
+        <p>about</p>
+        <p>contact</p>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
